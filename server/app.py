129150dd5ca44dc2d2ed846d44709cd4470f05cb
Brandon Collins <brandonc503@yahoo.com>
2024-09-19 03:53:19 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -3,3 +3,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# # append to server/app.py
+
+# @app.route('/<username>')
+# def user(username):
+#     return f'<h1>Profile for {username}</h1>'
+
+# modify user() in server/app.py
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# append to server/app.py
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
